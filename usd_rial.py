a421babd2dab1ee24ffffe997ad420513887637b
Ali <aliali.ali1378@yahoo.com>
2022-03-02 09:17:21 +0330

    get usd method updated

--- usd_rial.py
+++ usd_rial.py
@@ -1,12 +1,14 @@
 from requests import get
 import json
+from bs4 import BeautifulSoup
 
 def exchange(usd:float)->float:
-    url = "https://raters.ir/exchange/api/currency/usd"
+    url = "https://www.tgju.org/profile/price_dollar_rl"
     response = get(url)
-    data = json.loads(response.text)
-    price = data["data"]["prices"][0]["live"]
-    price = price.replace(",",'')
+    soup = BeautifulSoup(response.content, "html.parser")
+    result = soup.select("div.col-lg-12:nth-child(1) > div:nth-child(1) > div:nth-child(2) > table:nth-child(1) > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(2)")
+    result = result[0].text
+    price = result.replace(",",'')
     price = int(price)
     return usd*price
 
